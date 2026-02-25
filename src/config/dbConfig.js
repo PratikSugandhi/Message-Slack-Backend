01359aee38ce284e58439447a9c5769a06a94e3c
Pratik Sugandhi <pratiksugandhi4@gmail.com>
2026-02-25 12:48:02 +0530

    Added the dbConfig for mongo db connects

---
+++ src/config/dbConfig.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+import { DEV_DB_URL, NODE_ENV, PROD_DB_URL } from './serverConfig.js';
+
+// generally in industry there are two db development and production leverl so here it is logic that wheaterh db url is either of development or production 
+export default async function connectDB() {
+  try {
+    if (NODE_ENV === 'development') {
+      await mongoose.connect(DEV_DB_URL);
+    } else if (NODE_ENV === 'production') {
+      await mongoose.connect(PROD_DB_URL);
+    }
+    console.log(`Connected to mongodb database from ${NODE_ENV} environment`);
+  } catch (error) {
+    console.log('Error connecting to database', error);
+  }
+}
