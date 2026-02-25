0c3eaa1dadac422d14c24ee4abebe688294311cc
Pratik Sugandhi <pratiksugandhi4@gmail.com>
2026-02-25 13:00:34 +0530

    Added the routing layer in project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
-
+import apiRouter from './routes/apiRoutes.js'
 import connectDB from './config/dbConfig.js';
 import { PORT } from './config/serverConfig.js';
 
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api', apiRouter);
 
 app.get('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({ message: 'pong' });
