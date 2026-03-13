e40982f74da7935f34e3bc278f75222c37b5dd97
Pratik Sugandhi <pratiksugandhi4@gmail.com>
2026-03-13 17:04:18 +0530

    Added token logic

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -33,7 +33,7 @@ export const isAuthenticated = async (req, res, next) => {
     next();
   } catch (error) {
     console.log('Auth middleware error', error);
-    if (error.name === 'JsonWebTokenError') {
+    if (error.name === 'JsonWebTokenError' || error.name === 'TokenExpiredError') {
       return res.status(StatusCodes.FORBIDDEN).json(
         customErrorResponse({
           explanation: 'Invalid data sent from the client',
